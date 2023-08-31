0ddcb32690ccd53c9d6f4217422e2af8b03d56a5
Jeremy Hager <47301461+jeremyhager@users.noreply.github.com>
2023-08-31 10:42:37 -0700

    testing build (#1)
    * testing build
    
    * small fix for error checking

---
+++ cmd/pokemon.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jeremyhager/pokeapi/pokemon"
+	"github.com/spf13/cobra"
+)
+
+// pokemonCmd represents the pokemon command
+var pokemonCmd = &cobra.Command{
+	Use:   "pokemon",
+	Short: "A command to get pokemon.",
+	Long: `pokemon command is used for getting pokemon via the pokeapi either
+by id number or by name.
+
+Examples:
+pokego pokemon 1
+pokego pokemon bulbasaur
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			err := cmd.Help()
+			if err != nil {
+				log.Fatal(err)
+			}
+			os.Exit(0)
+		}
+		poke, err := pokemon.Get(args[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("pokemon info:\n%+v\n", poke.Species)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(pokemonCmd)
+}
